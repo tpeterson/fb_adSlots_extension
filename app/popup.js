f11be10ff4418967164e18a0030e1ca72d0e871c
Tim <tpeterson@users.noreply.github.com>
2016-11-29 18:20:13 -0800

    Redesign popup to look clean with tomato color, move button to bottom of link_feed and add average ad load to top of link_feed.

--- app/popup.js
+++ app/popup.js
@@ -10,13 +10,25 @@
       while (link_feed.firstChild) {
         link_feed.removeChild(link_feed.firstChild);
       }
+
+			var ad_slot_feed_el =  document.createElement('div');
+			ad_slot_feed_el.id = 'ad_slot_feed';
+
       var ad_slots = response;
       for (var i = 0; i < ad_slots.length; i++) {
-        var ad_slot = document.createElement('div');
+				var ad_slot = document.createElement('div');
         ad_slot.className = 'ad_slot';
-        ad_slot.textContent = 'Ad #' + ad_slots[i].ad_num + ' appears in News Feed slot #' + ad_slots[i].ad_pos;
-        link_feed.appendChild(ad_slot);
+        ad_slot.textContent = 'Ad #' + ad_slots[i].ad_num + ' => News Feed slot #' + ad_slots[i].ad_pos;
+        ad_slot_feed_el.appendChild(ad_slot);
       }
+
+			var post_ad_avg = (ad_slots[ad_slots.length-1].ad_pos)/ad_slots.length;
+			var ad_avg_el = document.createElement('div');
+			ad_avg_el.className = 'ad_avg';
+			ad_avg_el.textContent = 'Organic posts per ad ratio: ' + post_ad_avg;
+
+			link_feed.appendChild(ad_avg_el);
+			link_feed.appendChild(ad_slot_feed_el);
     }
   }
 
