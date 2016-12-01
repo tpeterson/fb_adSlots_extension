57351396e9d44a84170def90ba08f4cb5f1653cf
Tim <tpeterson@users.noreply.github.com>
2016-12-01 12:38:58 -0800

    Clean get_FbAdLinks to account for times when there are no ads. Load ad slots info when extension popup is opened and when button is clicked to find ads.

--- app/modules/get_FbAdLinks.js
+++ app/modules/get_FbAdLinks.js
@@ -1,15 +1,18 @@
 function get_FbAdLinks() {
   var sponsored_links = document.querySelectorAll('a.uiStreamSponsoredLink');
   var ad_positions = [];
-  for (var i=0; i < sponsored_links.length; i++) {
+
+  for (var i = 0; i < sponsored_links.length; i++) {
     var parsed_link = parseUri(sponsored_links[i]);
-    if (parsed_link.queryKey['ft[fbfeed_location]'] === '1') {
+    if ((parsed_link.queryKey['ft[fbfeed_location]'] === '1') && parsed_link.queryKey['ft[insertion_position]']) {
       var insertion_position = parsed_link.queryKey['ft[insertion_position]'];
       var ad_info = {
-        ad_num: parseInt(i, 10)+1,
-        ad_pos: parseInt(insertion_position, 10)+1
+        ad_num: parseInt(i, 10) + 1,
+        ad_pos: parseInt(insertion_position, 10) + 1
       };
       ad_positions.push(ad_info);
+    } else {
+      return ad_positions;
     }
   }
   return ad_positions;
--- app/popup.js
+++ app/popup.js
@@ -6,7 +6,7 @@
     if (response === 'Not Facebook') {
       explainer.textContent = 'Visit Facebook to see ad insertion positions';
     } else if (!response || response.length === 0) {
-      explainer.textContent = 'Reload Facebook to see ad slots';
+      explainer.textContent = 'No ads loaded yet';
     } else {
       while (link_feed.firstChild) {
         link_feed.removeChild(link_feed.firstChild);
@@ -14,30 +14,27 @@
 
       var ad_slots = response;
 
-			var ad_slot_feed_el = document.createElement('div');
-			ad_slot_feed_el.id = 'ad_slot_feed';
+      var ad_slot_feed_el = document.createElement('div');
+      ad_slot_feed_el.id = 'ad_slot_feed';
 
-			var post_ad_avg = (ad_slots[ad_slots.length - 1].ad_pos) / ad_slots.length;
-			var ad_avg_el = document.createElement('div');
-			ad_avg_el.className = 'ad_avg';
-			ad_avg_el.textContent = 'Organic posts per ad ratio: ' + post_ad_avg;
+      var post_ad_avg = Math.round(((ad_slots[ad_slots.length - 1].ad_pos) - ad_slots.length) / ad_slots.length);
+      var ad_avg_el = document.createElement('div');
+      ad_avg_el.className = 'ad_avg';
+      ad_avg_el.textContent = 'Organic posts per ad ratio: ' + post_ad_avg;
 
-			link_feed.appendChild(ad_avg_el);
-			link_feed.appendChild(ad_slot_feed_el);
+      link_feed.appendChild(ad_avg_el);
+      link_feed.appendChild(ad_slot_feed_el);
 
-			ad_slots.forEach(function(entry) {
-				var ad_slot_el = document.createElement('div');
-				ad_slot_el.className = 'ad_slot';
-				ad_slot_el.textContent = 'Ad #' + entry.ad_num + ' => News Feed slot #' + entry.ad_pos;
+      ad_slots.forEach(function(entry) {
+        var ad_slot_el = document.createElement('div');
+        ad_slot_el.className = 'ad_slot';
+        ad_slot_el.textContent = 'Ad #' + entry.ad_num + ' => News Feed slot #' + entry.ad_pos;
         ad_slot_feed_el.appendChild(ad_slot_el);
-				// setTimeout(() => {
-				// 	ad_slot_feed_el.appendChild(ad_slot_el);
-				// }, 150 * ad_slots.indexOf(entry));
-			});
+      });
     }
   }
 
-  window.document.getElementById('get_links').addEventListener('click', function() {
+  function requestLinks() {
     chrome.tabs.query({
         active: true,
         currentWindow: true
@@ -48,5 +45,13 @@
           subject: 'getLinks'
         }, setText);
       });
+  }
+
+  window.addEventListener('DOMContentLoaded', function() {
+    requestLinks();
+  });
+
+  window.document.getElementById('get_links').addEventListener('click', function() {
+    requestLinks();
   });
 })();
