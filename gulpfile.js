1a3f200b8e4c235363abc8e6fc3cc6565e35a159
Tim <tpeterson@users.noreply.github.com>
2017-02-15 11:13:26 -0800

    Add modules, gulpfile and babelrc to build js and css using gulp, babel, uglify, autoprefixer, sass, sourcemaps and newer.

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const newer = require('gulp-newer');
+
+gulp.task('scripts', function () {
+  return gulp.src('src/js/*.js')
+    .pipe(newer('public/js'))
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('styles', function () {
+  return gulp.src('src/css/*.scss')
+    .pipe(newer('public/css'))
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('default', ['scripts', 'styles']);
